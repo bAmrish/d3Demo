9dfb9cf3d019652a918a80eb36cc3815f5d003cc
Amrish Bharatiya <abharatiya@affinnova.com>
2014-04-28 11:52:08 -0400

    Added code for creating chart without chaining

--- scripts/d3demo.js
+++ scripts/d3demo.js
@@ -1,7 +1,12 @@
 window.addEventListener('load', function(){
+	makeChart1();
+	makeChart2();
+});
+
+var makeChart1 = function() {
 	var data = [4, 8, 15, 16, 23, 42];
 
-	d3.select("div.chart")
+	d3.select('div#chart1')
 		.selectAll('div')
 		.data(data)
 		.enter()
@@ -11,4 +16,18 @@ window.addEventListener('load', function(){
 		}).text (function(d) {
 			return d;
 		});
-});
+}
+
+var makeChart2 = function() {
+	var data = [4, 8, 15, 16, 23, 42];
+	var chart = d3.select('div#chart2');
+	var bars = chart.selectAll('div');
+	var barsUpdate = bars.data(data);
+	var barEnter = barsUpdate.enter().append('div');
+	barEnter.style('width', function(value){
+		return ((value * 10) + 'px');
+	});
+	barEnter.text(function (value) {
+		return value;
+	});
+}
