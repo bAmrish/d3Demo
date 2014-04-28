5f8573129342e8413974b40d9a40dc6d51b10be9
Amrish Bharatiya <abharatiya@affinnova.com>
2014-04-28 19:12:43 -0400

    first svg chart

--- scripts/d3demo.js
+++ scripts/d3demo.js
@@ -1,19 +1,26 @@
 window.addEventListener('load', function(){
 	makeChart1();
 	makeChart2();
+	makeChart3();
+	//svgChart1();
 });
 
 var makeChart1 = function() {
-	var data = [4, 8, 15, 16, 23, 42];
-
+	var data = [4, 8, 15, 16, 23, 42, 100 ];
+	var scale = d3.scale.linear()
+					.domain([0, d3.max(data)])
+					.range([0, 420]);
+ 
 	d3.select('div#chart1')
 		.selectAll('div')
 		.data(data)
 		.enter()
 		.append('div')
 		.style('width', function(d){
-			return ((d * 10) + 'px');
-		}).text (function(d) {
+			return (scale(d) + 'px');
+		})
+		.classed('bar', true)
+		.text (function(d) {
 			return d;
 		});
 }
@@ -30,4 +37,16 @@ var makeChart2 = function() {
 	barEnter.text(function (value) {
 		return value;
 	});
+}
+
+var makeChart3 = function() {
+	var data = [4, 8, 15, 16, 23, 42];
+	d3.select('div#chart3')
+		.selectAll('div')
+		.data(data)
+		.enter()
+		.append('div')
+		.text(function (d) {
+			return d
+		});
 }
