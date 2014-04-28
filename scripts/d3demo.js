5005d211f1500c59370cd71d4c62563d27e4d0c4
Sharvari Shah <sharvari.shah@aptecllc.com>
2014-04-28 10:26:30 -0400

    first chart

--- scripts/d3demo.js
+++ scripts/d3demo.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', function(){
 	console.log(d3)
+	var data = [4, 8, 15, 16, 23, 42];
+
+	d3.select("div.chart")
+		.selectAll('div')
+		.data(data)
+		.enter()
+		.append('div')
+		.style('width', function(d){
+			return ((d * 10) + 'px');
+		}).text (function(d) {
+			return d;
+		});
 });
