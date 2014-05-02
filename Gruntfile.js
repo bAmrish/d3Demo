a83b3b892aa4263207acc8ef080114084ac5a58f
Amrish Bharatiya <abharatiya@affinnova.com>
2014-05-02 09:21:43 -0400

    grunt serve port is now configurable through --port command

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,30 @@
 
 module.exports = function(grunt) {
 
+  	grunt.loadNpmTasks('grunt-contrib-connect');
+
+  	//capture --port option if passed in or elease default to 8000
+	var gruntServPort = grunt.option('port') || 8000;
+
   	// Project configuration.
   	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		connect: {
 			server: {
-				option: {
-					port: 8000
+				options: {
+					port: gruntServPort,
+					keepalive: true
 				}
 			}
 		}
 	});
 
-  	grunt.loadNpmTasks('grunt-contrib-connect');
-
   	//define grunt serve command to start a local server for dev testing.
-	grunt.registerTask('serve', ['connect:server:keepalive']);
+	grunt.registerTask('serve', ['connect:server']);
 
 	//default task.
 	grunt.registerTask('default', function () {
-		console.log('Use "grunt serve" command to start local server on port 8000. ');
+		console.log('Use "grunt serve" command to start local server on port ' + gruntServPort + '. ');
 	});
 
 };
