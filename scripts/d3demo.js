a629bfe97f12be242781186fb56e7c9f3ef79ce7
Amrish Bharatiya <abharatiya@affinnova.com>
2014-05-02 17:06:03 -0400

    Moved all the chart rendering code to BarChart class

--- scripts/d3demo.js
+++ scripts/d3demo.js
@@ -1,40 +1,14 @@
 'use strict';
 
 window.addEventListener('load', function () {
-    chart();
-});
-
-var chart = function () {
-    var containerWidth = 1024;
-    var containerHeight = 500;
-    var margin = {
-        top: 30, right: 30, bottom: 120, left: 30
+    var config = {
+        width: 1024,
+        height: 500,
+        margin : {
+            top: 30, right: 30, bottom: 120, left: 30
+        }
     };
-
-    var chartWidth = containerWidth - margin.left - margin.right;
-    var chartHeight = containerHeight - margin.top - margin.bottom;
-
-    var y_scale = d3.scale.linear()
-        .range([chartHeight, 0]);
-
-    var x_scale = d3.scale.ordinal()
-        .rangeRoundBands([0, chartWidth], 0.1);
-
-    var x_axis = d3.svg.axis()
-        .scale(x_scale)
-        .orient('bottom');
-
-    var y_axis = d3.svg.axis()
-        .scale(y_scale)
-        .orient('left')
-        .ticks(10, '0.0s');
-
-    var chart = d3.select('#vbar1')
-        .attr('width', containerWidth)
-        .attr('height', containerHeight)
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
+    var b = new BarChart('#vbar1', config);
     var type = function (d) {
         d.value = parseFloat(d.CENSUS2010POP, 10);
         d.name = d.NAME;
@@ -49,18 +23,87 @@ var chart = function () {
             return b.value - a.value;
         });
 
-        x_scale.domain(data.map(function (d) {
+        b.update(data);
+
+    });
+    //chart(config);
+});
+
+function BarChart(node, config) {
+    this.config = config;
+    this.node = d3.select(node);
+    this.init();
+}
+
+BarChart.prototype = {
+
+    init: function () {
+
+        this.containerWidth = this.config.width || 1024;
+        this.containerHeight = this.config.height || 500;
+
+        this.margin = this.config.margin || {
+            top: 30, right: 30, bottom: 120, left: 30
+        };
+
+        this.chartWidth = this.containerWidth - this.margin.left - this.margin.right;
+        this.chartHeight = this.containerHeight - this.margin.top - this.margin.bottom;
+
+        this.createScales().createAxis().createChart();
+
+        return this;
+    },
+
+    createScales: function() {
+        this.y_scale = d3.scale.linear()
+            .range([this.chartHeight, 0]);
+
+        this.x_scale = d3.scale.ordinal()
+            .rangeRoundBands([0, this.chartWidth], 0.1);
+
+        return this;
+    },
+
+    createAxis: function() {
+        this.x_axis = d3.svg.axis()
+            .scale(this.x_scale)
+            .orient('bottom');
+
+        this.y_axis = d3.svg.axis()
+            .scale(this.y_scale)
+            .orient('left')
+            .ticks(10, '0.0s');
+
+        return this;
+    },
+
+    createChart: function() {
+        this.chart = this.node
+            .attr('width', this.containerWidth)
+            .attr('height', this.containerHeight)
+            .append('g')
+            .attr('transform', 'translate(' + this.margin.left + ', ' + this.margin.top + ')');
+
+        return this;
+    },
+
+    update: function(data){
+        var barChart = this;
+
+        this.data = data;
+
+        this.x_scale.domain(data.map(function (d) {
             return d.name;
         }));
 
-        y_scale.domain([0, d3.max(data, function (d) {
+        this.y_scale.domain([0, d3.max(data, function (d) {
             return d.value;
         })]);
 
-        chart.append('g')
-            .attr('transform', 'translate(' + 0 + ', ' + chartHeight + ')')
+        this.chart.append('g')
+            .attr('transform', 'translate(' + 0 + ', ' + this.chartHeight + ')')
             .attr('class', 'x axis')
-            .call(x_axis)
+            .call(this.x_axis)
             .selectAll('.tick text')
             .call(function (texts) {
                 texts.each(function () {
@@ -68,14 +111,14 @@ var chart = function () {
                         .attr('style', 'text-anchor:start')
                         .attr('transform', 'rotate(90)')
                         .attr('dy', 0)
-                        .attr('y', x_scale.rangeBand() * 1 / 4)
+                        .attr('y', barChart.x_scale.rangeBand() * 1 / 4)
                         .attr('dx', '0.7em');
                 });
             });
 
-        chart.append('g')
+        this.chart.append('g')
             .attr('class', 'y axis')
-            .call(y_axis)
+            .call(this.y_axis)
             .append('text')
             .text('Population')
             .style('text-anchor', 'end')
@@ -84,20 +127,38 @@ var chart = function () {
             .attr('y', -10)
             .attr('x', 50);
 
-        var bars = chart.selectAll('g.bar')
-            .data(data)
+        this.bars = this.chart.selectAll('g.bar')
+            .data(this.data)
             .enter()
             .append('g')
             .attr('class', 'bar')
             .attr('transform', function (d) {
-                return 'translate(' + x_scale(d.name) + ',' + 0 + ')';
+                return 'translate(' + barChart.x_scale(d.name) + ',' + 0 + ')';
             })
             .append('rect')
-            .attr('width', x_scale.rangeBand())
+            .attr('width', this.x_scale.rangeBand())
             .attr('height', 1)
-            .attr('y', chartHeight);
+            .attr('y', this.chartHeight);
 
-        bars.on('mouseover', function (d, i) {
+        this.bars.transition()
+            .duration(800)
+            .attr('y', function (d) {
+                return barChart.y_scale(d.value);
+            })
+            .attr('height', function (d) {
+                return barChart.chartHeight - barChart.y_scale(d.value);
+            });
+
+        this.createToolTip();
+
+        return this;
+    },
+
+    createToolTip: function() {
+
+        var barChart = this;
+
+        this.bars.on('mouseover', function (d, i) {
             var tooltip_bottom_padding = 10;
             d3.select(this.parentNode)
                 .append('g')
@@ -105,7 +166,7 @@ var chart = function () {
                 .append('text')
                 .text(d.name + ' = ' + d3.format('.3s')(d.value))
                 .attr('x', 0)
-                .attr('y', y_scale(d.value) - tooltip_bottom_padding);
+                .attr('y', barChart.y_scale(d.value) - tooltip_bottom_padding);
 
             var textWidth = $(d3.select('.bar-tooltip').select('text').node()).width();
             var textHeight = $(d3.select('.bar-tooltip').select('text').node()).height();
@@ -115,10 +176,9 @@ var chart = function () {
                 .attr('width', textWidth + 10)
                 .attr('height', textHeight + 5)
                 .attr('x', (textWidth / -2) - 5)
-                .attr('y', y_scale(d.value) - tooltip_bottom_padding - textHeight)
+                .attr('y', barChart.y_scale(d.value) - tooltip_bottom_padding - textHeight)
                 .attr('rx', 5)
-                .attr('ry', 5)
-            ;
+                .attr('ry', 5);
 
             if (i === 0) {
                 d3.select('.bar-tooltip text').attr('class', 'first');
@@ -126,18 +186,13 @@ var chart = function () {
             }
         });
 
-        bars.on('mouseout', function () {
+        this.bars.on('mouseout', function () {
             d3.select(this.parentNode).select('.bar-tooltip').remove();
         });
+    }
+};
 
-        bars.transition()
-            .duration(800)
-            .attr('y', function (d) {
-                return y_scale(d.value);
-            })
-            .attr('height', function (d) {
-                return chartHeight - y_scale(d.value);
-            });
+var chart = function (config) {
 
-    });
+
 };
